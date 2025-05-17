1ec2e53f583a2a5ee2539c178805e88f4e587f85
TheHappyLife <dangnguyen120699@gmail.com>
2025-05-17 14:48:15 +0700

    new version

--- src/components/InputKeyboard/components/Input/index.tsx
+++ src/components/InputKeyboard/components/Input/index.tsx
@@ -1,12 +1,4 @@
-import {
-  forwardRef,
-  useCallback,
-  useEffect,
-  useImperativeHandle,
-  useMemo,
-  useRef,
-  useState,
-} from "react";
+import { forwardRef, useCallback, useEffect, useImperativeHandle, useMemo, useRef, useState } from "react";
 import { InputProps, InputRef } from "./type";
 import Cursor from "./Components/Cusor";
 import {
@@ -41,10 +33,7 @@ const Input = forwardRef<InputRef, InputProps>((props, ref) => {
   } = props;
   const isEmpty = !displayValue || displayValue.length === 0;
 
-  const themeValues = useMemo(
-    () => themeValuesOverride ?? INPUT_THEME[theme],
-    [theme, themeValuesOverride]
-  );
+  const themeValues = useMemo(() => themeValuesOverride ?? INPUT_THEME[theme], [theme, themeValuesOverride]);
 
   const focus = () => {
     setTimeout(() => {
